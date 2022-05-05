832d16661d848386e05b6def68aa6fd192e2f10a
NirmalHNanera <94095079+NirmalHNanera@users.noreply.github.com>
2022-05-05 09:09:42 +0530

    Add files via upload

---
+++ ProducerConsumerDemo.java
@@ -0,0 +1,71 @@
+class MyHeadPhone{
+    public static int stock=5;
+    void producer() {
+        while (true) {
+            stock++;
+            System.out.println("Producer = " + stock);
+            try {
+                Thread.sleep((long) Math.random()*300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if(stock>5){
+                try {
+                    Thread.sleep(800);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    void consumer(){
+        while (true) {
+            stock--;
+            System.out.println("Consumer = " + stock);
+            try {
+                Thread.sleep((long) Math.random()*200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if(stock<1){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+class MyProducer extends Thread{
+    MyHeadPhone obj = new MyHeadPhone();
+    public void run(){
+        try {
+            obj.producer();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+class MyConsumer extends Thread{
+    MyHeadPhone obj = new MyHeadPhone();
+    public void run(){
+        try {
+            obj.consumer();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
+
+public class ProducerConsumerDemo{
+    public static void main(String[] args) {
+        MyProducer obj1 = new MyProducer();
+        MyConsumer obj2 = new MyConsumer();
+        obj1.start();
+        obj2.start();
+    }
+}
